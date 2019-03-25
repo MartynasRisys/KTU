73ee6d82e22e84599bd4ff76f5d62161f5a80eb5
JasonasIgn <ignas.jasonas@gmail.com>
2019-03-25 17:29:10 +0200

    Add kriptovaliutu pinigine

--- 3 kursas/Programavimo kalbu teorija/lab 2 (neatsiskaityta)/Scalatron/users/ignjsn/versions/0/Bot.scala
+++
@@ -1,9 +0,0 @@
-// this is the source code for your bot - have fun!
-
-class ControlFunctionFactory {
-    def create = new Bot().respond _
-}
-
-class Bot {
-    def respond(input: String) = "Status(text=ignjsn)"
-}
