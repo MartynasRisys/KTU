3d373301c3da1c85d53ffbd9c3d5ace24e6cba32
Ignas <ignas.jasonas@telesoftas.com>
2019-05-27 13:19:14 +0300

    Finish task

--- 3 kursas/II semestras/Programavimo kalbu teorija/lab 3/3.hs
+++ 3 kursas/II semestras/Programavimo kalbu teorija/lab 3/3.hs
@@ -9,8 +9,8 @@ main = do
         let singlewords = words contents
             list = f singlewords
         print list
-        let listLength = length list
-        let a = getMedian list listLength
+        writeFile "output.txt" ""
+        iterateAllList list 1
         hClose handle
         
 
@@ -21,7 +21,19 @@ getMedian :: [Int] -> Int -> Float
 getMedian list len = do
     if (len `mod` 2 == 0)
         then divideIntToFloat (list!!(len `div` 2 - 1) + list!!(len `div` 2)) 2
-    else divideIntToFloat (list!!(len - 1)) 2
+    else divideIntToFloat (list!!((len - 1) `div` 2)) 1
 
 divideIntToFloat :: Int -> Int -> Float
 divideIntToFloat a b = (fromIntegral a) / (fromIntegral b)
+
+iterateAllList list currentIteration = do
+    let lengthList = length list
+    if (currentIteration <= lengthList)
+        then do
+            let median = getMedian list currentIteration
+            let roundedMedian = floor median
+            appendFile "output.txt" (show roundedMedian)
+            appendFile "output.txt" ("\n")
+            iterateAllList list (currentIteration + 1)
+    else return ()
+
